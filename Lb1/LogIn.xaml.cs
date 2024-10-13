42bcfe1f5f53e67430b78bcf2b49bcd622ccdbf3
Mishanyyy228 <mananev13@gmail.com>
2024-10-14 00:19:17 +0300

    Add files via upload

---
+++ Lb1/LogIn.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lb1
+{
+    /// <summary>
+    /// Логика взаимодействия для LogIn.xaml
+    /// </summary>
+    public partial class LogIn : Window
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+            Pochta_user11.Text = "mananev13@gmal.com";
+            Password_user11.Text = "Введите пароль";
+        }
+
+        private void Pochta_user11_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Pochta_user11.Text == "mananev13@gmal.com")
+            {
+                Pochta_user11.Text = string.Empty;
+                Pochta_user11.Foreground = Brushes.Black;
+            }
+        }
+
+        private void Password_user11_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Password_user11.Text == "Введите пароль")
+            {
+                Password_user11.Text = string.Empty;
+                Password_user11.Foreground = Brushes.Black;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window w2 = new Registration();
+            Hide();
+            w2.Show();
+        }
+    }
+}
---
+++ Lb1/Registration.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lb1
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+        public Registration()
+        {
+            InitializeComponent();
+            Name_user.Text = "Введите имя пользователя";
+            Pass_user.Text = "Введите пароль";
+            Email_user.Text = "Введите почту";
+            Pass_user1.Text = "Повторите пароль";
+        }
+
+        private void Name_user_GotFocus_1(object sender, RoutedEventArgs e)
+        {
+            if (Name_user.Text == "Введите имя пользователя")
+            {
+                Name_user.Text = string.Empty;
+                Name_user.Foreground = Brushes.Black;
+            }
+        }
+
+        private void Email_user_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Email_user.Text == "Введите почту")
+            {
+                Email_user.Text = string.Empty;
+                Email_user.Foreground = Brushes.Black;
+            }
+        }
+
+        private void Pass_user_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Pass_user.Text == "Введите пароль")
+            {
+                Pass_user.Text = string.Empty;
+                Pass_user.Foreground = Brushes.Black;
+            }
+        }
+
+        private void Pass_user1_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Pass_user1.Text == "Повторите пароль")
+            {
+                Pass_user1.Text = string.Empty;
+                Pass_user1.Foreground = Brushes.Black;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window w2 = new LogIn();
+            Hide();
+            w2.Show();
+        }
+    }
+}
